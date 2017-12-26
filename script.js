9201e440f3f8fd53d00446e61dde58fc132b3c1d
khomdeveloper <34720183+khomdeveloper@users.noreply.github.com>
2017-12-26 15:27:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -41,3 +41,19 @@ var Main = {
   }
 
 };
+
+var Data = {
+ 
+  get : function(){
+     return {
+              description : Main.truncateString('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+                                                80),
+              image : 'http://bm.img.com.ua/nxs/img/prikol/images/large/3/9/315193.jpg',
+              username : 'user Name',
+              score1 : Math.round(Math.random()*100)/10 + '',
+              score2 : Math.round(Math.random()*100) + '',
+              price : Math.round(Math.round()*100) + ''
+            };
+  }
+  
+}  
