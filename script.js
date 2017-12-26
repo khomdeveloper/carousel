90a0f63d69c6f20e02ce0db3a1e4802f131b8c01
khomdeveloper <34720183+khomdeveloper@users.noreply.github.com>
2017-12-26 15:56:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -44,8 +44,8 @@ var Main = {
 
 var Data = {
  
-  get : function(){
-     return {
+  get : function(data){
+     var result = {
               description : Main.truncateString('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                                                 80),
               image : 'http://bm.img.com.ua/nxs/img/prikol/images/large/3/9/315193.jpg',
@@ -54,6 +54,13 @@ var Data = {
               score2 : Math.round(Math.random()*100) + '',
               price : Math.round(Math.random()*100) + ''
             };
+    
+    for (key in data){
+      result[key] = data[key];  
+    }
+   
+    return result;
+    
   }
   
 }  
